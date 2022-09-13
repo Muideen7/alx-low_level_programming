3648791d82261fd4b15143df6da9b2e44cbc669e
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-09-14 00:42:49 +0100

    Update-advanced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Print the sum of even Fibonacci numbers.
- * not exceeding 4,000,000.
- * Return: 0
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i = 1, j = 2, total = 0;
-	int k;
+	int c;
+	long int n1, n2, fn;
 
-	while (j < 4000000)
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
 	{
-		if (j % 2 == 0)
-			total += j;
-		k = j;
-		j += i;
-		i = k;
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
 	}
-	printf("%d\n", total);
+	printf("\n");
 	return (0);
 }
