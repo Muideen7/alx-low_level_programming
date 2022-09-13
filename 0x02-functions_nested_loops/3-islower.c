69222b2dede43c1f0d61c2ccb31923a8285adf85
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-09-13 21:54:53 +0100

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c) 
+{
+	char c;
+
+	  if ( c >= 'a' && c <= 'z')
+     {
+         _putchar(c);
+     }
+     
+     
+           _putchar('\n');
+}
