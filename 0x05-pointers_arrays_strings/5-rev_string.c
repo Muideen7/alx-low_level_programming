3655893c50ec7cb79c1ce31c05bedf36cf299d6f
Olayeye Muideen A <olayeyeayomide2000@gmail.com>
2022-09-20 00:32:24 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,8 +11,8 @@ void rev_string(char *s)
 	char m;
 	int i, middle;
 
-	1 = strlen(S);
-	for (i = 0; i < middle / 2; i++)
+	middle = strlen(S);
+	for (i = 0; i < 1 / 2; i++)
 	{
 		m = s[i];
 		s[i] = s[middle - 1 - i];
