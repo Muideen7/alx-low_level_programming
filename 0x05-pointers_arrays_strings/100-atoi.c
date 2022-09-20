f98d29ba95a7367fa36afbd76f5115b1cb517ff3
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-09-20 01:23:29 +0100

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert string to int
+ * @s: assigned int
+ * Return: int
+*/
+
+int _atoi(char *s)
+{
+	int m = 0;
+
+	if (*s == m)
+	{
+	_putchar(m);
+	}
+	return (0);
+}
