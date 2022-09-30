afdb3befbbd80952b6eb821ea453fb5b8d6c1ca5
Olayeye Muideen A <olayeyeayomide2000@gmail.com>
2022-09-30 23:31:38 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,10 +6,19 @@
  * Return: 0 - success
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void)argv;
+	int i;
 
-	printf("%d\n", argc - 1);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
 	return (0);
 }
