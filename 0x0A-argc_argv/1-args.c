7939ca5539bf87b2d63fadeda7f1816b32212dd2
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-09-30 22:16:42 +0100

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 #include <Stdio.h>
 /**
  * main - prints the number of arguemenys passed into it
- * @argc: count the number of arguemeny in the command line
+ * @argc: count the number of arguement in the command line
  * @argv: gives the array of the arguement
  * Return: 0 - success
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("argc = %d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
