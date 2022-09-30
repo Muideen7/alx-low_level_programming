75526b86e5b01bee4f4f35a35623914a37f46f70
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-09-30 23:48:31 +0100

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,24 +1,15 @@
-#include <Stdio.h>
+#include <stdio.h>
 /**
- * main - prints the number of arguemenys passed into it
- * @argc: arguement counter
- * @argv: arguement vector 
- * Return: 0 - success
+ * main - prints the number of arguments pass into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
+	(void)argv;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
