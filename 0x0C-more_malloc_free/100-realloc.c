11722405a750e2c4393f66e4521a089c0be25462
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-06 21:56:01 +0100

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 /**
  * _realloc - reallocates a memory block.
@@ -9,7 +9,8 @@
  * Return: ptr.
  * if new_size == old_size, returns ptr without changes.
  * if malloc fails, returns NULL.
- */
+*/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
@@ -19,10 +20,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
+	{
 		ptr = malloc(new_size);
+	}
 
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 
 	free(ptr);
 	ptr = malloc(new_size);
