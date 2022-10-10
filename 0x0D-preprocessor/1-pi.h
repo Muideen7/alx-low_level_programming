5182f7ed5b441307371ae75d685616dfbf031ce4
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-10 21:06:06 +0100

    Task 1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,10 @@
 #ifndef PI_H
 #define PI_H
+
 /**
- * define - header  function 
- * PI: macro name
- * DESC: token_value 3.14...
+ * define - header funtion
+ * PI: Macro Name
+ * DESC: token_value 3.1415...
 */
 
 #define PI 3.14159265359
