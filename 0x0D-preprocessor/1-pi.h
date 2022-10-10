15e40c300a89e6a6d953ff12beb30909c528780e
Olayeye Muideen A <olayeyeayomide2000@gmail.com>
2022-10-10 20:25:05 +0100

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,11 @@
-#ifndef PI
-#define PI
-
-#define PI 3.14159265359
-
-#endif
+#ifndef PI_H 
+#define PI_H 
+ /** 
+  * define - main funtion 
+  * PI: Macro Name 
+  * DESC: token_value 3.1415... 
+ */ 
+  
+ #define PI 3.14159265359 
+  
+ #endif
