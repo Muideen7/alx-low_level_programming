505d921befa1715374a09c4d3ca22e96e0fb5656
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-21 02:04:46 +0100

    lists

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,5 +1,1 @@
-# Singly linked lists in C programming 
-All files are written in C language
-Using Ubuntu 22.04 Terminal 
-Gcc version 9.1
-
+This project focus on singly linked lists.
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,7 +9,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
-*/
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -15,12 +18,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-#include <stdlib.h>
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+size_t list_len_recursion(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif
+#endif /* _LISTS_H_ */
