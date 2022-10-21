5a7794d3631e541e2ee218a2199d162a2bdb54cc
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-21 01:57:38 +0100

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#ifndef LIST_H
+#define LIST_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,6 +14,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+#include <stdlib.h>
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
