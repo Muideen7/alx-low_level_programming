bf0273ceee6b84955df8fdf91b9a9a615908175e
Olayeye Muideen Ayomide <olayeyeayomide2000@gmail.com>
2022-10-21 03:40:17 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,20 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
-/**
- * File: lists.h
- * Auth: Olayeye Muideen
- * Desc: Header file containing prototypes and definitions for all functions
- *
+
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
 */
+
 #include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - malloc
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Desc: singly linked list node structure
+ * Description: singly linked list node structure
+ *              for Holberton project
 */
 typedef struct list_s
 {
