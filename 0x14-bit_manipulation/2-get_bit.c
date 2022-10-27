d59748f78ef02c4480da346bb3c8673d8b13c809
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-27 08:57:42 +0100

    Bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * get_bit - returns the value of a bit at a given
@@ -7,7 +7,7 @@
  * @index: index of the bit.
  *
  * Return: value of the bit.
- */
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
