35dfead7dffc1bf2472774ced7544330e8a48997
Yungmaniie <olayeyeayomide2000@gmail.com>
2022-10-27 08:59:07 +0100

    Bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * flip_bits - returns the number of bits you would
@@ -7,7 +7,7 @@
  * @m: number two.
  *
  * Return: number of bits.
- */
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int nbits;
