30a31f1289014ea9aa615e30f1d777e374b8764f
muideen7 <olayeyeayomide2000@gmail.com>
2022-10-31 23:03:04 +0100

    I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * read_textfile - function that reads a text file and prints it
- * @filename: pointer char
- * @letters: size 
- * Return: NULL
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
